69445c95db14a58e25b85ec92217bdee06e08f32
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-04-29 17:22:09 -0400

    created function that deletes tasks

--- main.js
+++ main.js
@@ -82,3 +82,15 @@ let resetForm = () => {
   dateInput.value = "";
   textarea.value = "";
 };
+
+// Delete tasks
+
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+
+  data.splice(e.parentElement.parentElement.id, 1);
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+};
