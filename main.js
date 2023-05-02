61cfc7750ed97169b4f4a3eebb186a6e572abf69
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-05-01 21:33:24 -0400

    added function to edit tasks

--- main.js
+++ main.js
@@ -94,3 +94,15 @@ let deleteTask = (e) => {
 
   console.log(data);
 };
+
+// Edit tasks
+
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+};
